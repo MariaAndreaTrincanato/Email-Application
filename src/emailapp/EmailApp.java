c199b63538f329a54ee9072668f8dc66ca00d372
MariaAndrea <trincanato.mariaandrea3@gmail.com>
2020-01-22 22:16:47 +0100

    Project done
    Completed the poject

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,7 +4,10 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Email em1 = new Email("John", "Smith");
+		
+		em1.setAlternateEmail("mymail@gmail.com");
+		System.out.print(em1.showInfo());
 	}
 
 }
